2f1f6a2d1cbb586f7070f1aef3b310f7f3172c62
WhatsWrongOB <148737556+WhatsWrongOB@users.noreply.github.com>
2023-12-20 10:26:40 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+const boxes = document.querySelectorAll('.box')
+const winner = document.querySelector('.winner')
+const reset = document.querySelector('.reset')
+
+
+let winningPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+const checkWinner = () => {
+    for (let pattern of winningPatterns) {
+        const [a, b, c] = pattern;
+        if (boxes[a].textContent && boxes[a].textContent === boxes[b].textContent && boxes[b].textContent === boxes[c].textContent) {
+            if (boxes[a].textContent === 'O') {
+                winner.textContent = `Congratulation O Wins!`;
+                winner.style.color = 'green'
+            }
+            else {
+                winner.textContent = `Congratulation X Wins!`;
+                winner.style.color = 'blue'
+            }
+            boxes.forEach((box) => {
+                box.disabled = true
+
+            })
+            break;
+        }
+    }
+}
+
+let box0 = true
+
+boxes.forEach((box) => {
+
+    box.addEventListener('click', () => {
+
+        if (box0) {
+            box.textContent = 'O'
+            box.style.color = 'green'
+            box0 = false
+        }
+        else {
+            box.textContent = 'X'
+            box.style.color = 'blue'
+            box0 = true
+        }
+        box.disabled = true
+        checkWinner()
+    })
+
+    reset.addEventListener('click', () => {
+        box.textContent = ''
+        winner.textContent = ''
+        box.disabled = false
+
+    })
+})
+
+
+
